921e2c1e2cb2a46faf89248930d2b9034bc85a60
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-01-27 10:10:29 +0530

    leetcode problem no 1342

---
+++ NoStepstomakeanumberzero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NoStepstomakeanumberzero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(numberOfSteps(number));
+
+    }
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                steps++;
+            } else {
+                num = num - 1;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
