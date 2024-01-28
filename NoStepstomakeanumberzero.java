f18c681efe67f4611ac3fba546d9469385ef1c52
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-01-28 11:31:05 +0530

    problem no 1342

--- NoStepstomakeanumberzero.java
+++ NoStepstomakeanumberzero.java
@@ -11,10 +11,10 @@ public class NoStepstomakeanumberzero {
         int steps = 0;
         while (num != 0) {
             if (num % 2 == 0) {
-                num = num / 2;
+                num /= 2;
                 steps++;
             } else {
-                num = num - 1;
+                num -= 1;
                 steps++;
             }
         }
