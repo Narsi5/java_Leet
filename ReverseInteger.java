71a1346451fb7c06d9623d759000d3db41f8992a
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-01-30 21:31:49 +0530

    problem no 7

---
+++ ReverseInteger.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(reverse(n));
+    }
+    public static int reverse(int x) {
+        long normal_num = 0;
+
+        while (x != 0) {
+            normal_num = normal_num * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (normal_num < Integer.MIN_VALUE || normal_num > Integer.MAX_VALUE) ? 0 : (int) normal_num;
+    }
+}
