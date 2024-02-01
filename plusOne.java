1111d3bf6dcf541a3515737df580eee66dc87ac5
Narsi5 <123388559+Narsi5@users.noreply.github.com>
2024-02-01 21:21:51 +0530

    problem no 66

--- plusOne.java
+++ plusOne.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class plusOne {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array");
         int k = scanner.nextInt();
