d4172c4bd5dbe0c5267af032685b43e3b019b000
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-02-02 21:07:49 +0530

    problem no 58

---
+++ LengthofthelastWord.java
@@ -0,0 +1,15 @@
+public class LengthofthelastWord {
+    public static void main(String[] args) {
+        String input = " hello world oh ";
+        System.out.println(LastWord(input));
+    }
+    public static int LastWord(String s) {
+        int lastword = s.length() -1;
+        while(lastword>=0 && s.charAt(lastword)==' ')
+            --lastword;
+        int j=lastword;
+        while(j>=0 && s.charAt(j) != ' ')
+            --j;
+        return lastword-j;
+    }
+}
