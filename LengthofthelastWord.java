b364f079923f7515329e80b622a996622cef15a7
Narsi5 <123388559+Narsi5@users.noreply.github.com>
2024-02-02 21:22:03 +0530

    Update LengthofthelastWord.java

--- LengthofthelastWord.java
+++ LengthofthelastWord.java
@@ -5,8 +5,8 @@ public class LengthofthelastWord {
     }
     public static int LastWord(String s) {
         int lastword = s.length() -1;//give the total length of the string 
-        while(lastword>=0 && s.charAt(lastword)==' ')
-            --lastword;
+        while(lastword>=0 && s.charAt(lastword)==' ')//if the lastcharacter is space then it decreases the length of the string 
+            --lastword;//it repeats untill the last char is not zero
         int j=lastword;
         while(j>=0 && s.charAt(j) != ' ')
             --j;
