a59ae1000aed20da1c6540d7553abe0e40faa724
Narsi5 <123388559+Narsi5@users.noreply.github.com>
2024-02-04 06:11:15 +0530

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -32,20 +32,24 @@ public class AddBinary {
             }
             if (sum == 0 || sum == 1) {
                 result.append(sum);//it will 0 or 1 depending on the the sum to the result String 
-                carry = 0;
+                carry = 0;//as sum is 1or 0 from the above table we can make carry =0 
             }
             else if (sum == 2) {
-                result.append("0");
+                result.append("0");//if sum is 2 it means it is 1+1 case so we will have carry = 1 and in the result string 0 will be added
                 carry = 1;
             }
             else {
+                //this is when u have to add 1+1 and have a carry from previous as 1
+                //example : 11
+                //         +11
+                //         110 
                 result.append("1");
                 carry = 1;
             }
             i--;
             j--;
         }
-        if (carry == 1) {
+        if (carry == 1) {// after adding all if have a carry then we hae to add to the result string
             result.append("1");
         }
         return result.reverse().toString();
