6bdbfefceda28d9c791cf2f438e7dea845ddb0d0
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-02-04 05:51:18 +0530

    problem no 67

---
+++ AddBinary.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class AddBinary {
+    public static void main(String[] args) {
+
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String a= sc.nextLine();
+        String b= sc.nextLine();
+        System.out.println(add(a, b));
+
+    }
+    public static String add(String a, String b) {
+        //        sum   carry
+        // 0+0     0      0
+        // 0+1     1      0
+        // 1+0     1      0
+        // 1+1     0      1
+
+        int i = a.length() - 1, j = b.length() - 1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+            }
+            if (sum == 0 || sum == 1) {
+                result.append(sum);
+                carry = 0;
+            }
+            else if (sum == 2) {
+                result.append("0");
+                carry = 1;
+            }
+            else {
+                result.append("1");
+                carry = 1;
+            }
+            i--;
+            j--;
+        }
+        if (carry == 1) {
+            result.append("1");
+        }
+        return result.reverse().toString();
+
+    }
+}
