57862278ab5e29347734081a61cdad0bd123b9aa
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-04-30 11:04:35 +0530

    problem no 24

---
+++ SwapNodesinPairs.java
@@ -0,0 +1,19 @@
+public class SwapNodesinPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while(prev.next != null && prev.next.next != null){
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+
+            prev = first;
+        }
+        return dummy.next;
+    }
+
+}
