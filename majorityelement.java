bbe9d5a75a17a4747be094867dca4d0e744acd9b
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-02 21:37:40 +0530

    problem no 169

---
+++ majorityelement.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class majorityelement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        int ans =0;
+        for(int key :map.keySet()){
+            if(map.get(key)>n/2){
+                ans = key  ;
+            }
+        }
+        return ans;
+
+    }
+
+}
