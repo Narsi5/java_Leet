315cb7643880b25b581ee29e5ec54a578c7125a3
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-10 12:50:57 +0530

    problem no 341

---
+++ NestedListIterator.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+public class NestedListIterator {
+    public class NestedIterator implements Iterator<Integer> {
+        List<Integer> flatList;
+        int iterator;
+        public NestedIterator(List<NestedInteger> nestedList) {
+            flatList = new ArrayList<>();
+            iterator = 0;
+            flattenList(nestedList);
+        }
+        private void flattenList(List<NestedInteger> nestedList){
+            for(NestedInteger ele : nestedList){
+                if(ele.isInteger()){
+                    flatList.add(ele.getInteger());
+                }else{//recurse for the flatten list function
+                    flattenList(ele.getList());
+                }
+            }
+        }
+        @Override
+        public Integer next() {
+            if(hasNext()){
+                return flatList.get(iterator++);
+            }else{
+                return null;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator < flatList.size();
+        }
+    }
+}
