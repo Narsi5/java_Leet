f9a12b738ad6d0c4d850c719d3bcd76fe8790037
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-12 21:06:41 +0530

    problem no 341

--- NestedListIterator.java
+++ NestedListIterator.java
@@ -1,7 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 public class NestedListIterator {
-    public class NestedIterator implements Iterator<Integer> {
+    public static class NestedIterator implements Iterator<Integer> {
         List<Integer> flatList;
         int iterator;
         public NestedIterator(List<NestedInteger> nestedList) {
@@ -30,6 +31,66 @@ public class NestedListIterator {
         @Override
         public boolean hasNext() {
             return iterator < flatList.size();
+        }
+    }
+    class NestedInteger {
+        private List<NestedInteger> list;
+        private Integer integer;
+
+        public NestedInteger(List<NestedInteger> list){
+            this.list = list;
+        }
+
+        public void add(NestedInteger nestedInteger) {
+            if(this.list != null){
+                this.list.add(nestedInteger);
+            } else {
+                this.list = new ArrayList();
+                this.list.add(nestedInteger);
+            }
+        }
+
+        public void setInteger(int num) {
+            this.integer = num;
+        }
+
+        public NestedInteger(Integer integer){
+            this.integer = integer;
+        }
+
+        public NestedInteger() {
+            this.list = new ArrayList();
+        }
+
+        public boolean isInteger() {
+            return integer != null;
+        }
+
+        public Integer getInteger() {
+            return integer;
+        }
+
+        public List<NestedInteger> getList() {
+            return list;
+        }
+
+        public String printNi(NestedInteger thisNi, StringBuilder sb){
+            if(thisNi.isInteger()) {
+                sb.append(thisNi.integer);
+                sb.append(",");
+            }
+            sb.append("[");
+            for(NestedInteger ni : thisNi.list){
+                if(ni.isInteger()) {
+                    sb.append(ni.integer);
+                    sb.append(",");
+                }
+                else {
+                    printNi(ni, sb);
+                }
+            }
+            sb.append("]");
+            return sb.toString();
         }
     }
 }
