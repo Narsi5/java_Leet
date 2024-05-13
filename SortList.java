e7358b8b1aeef209233e8ba8e2fd5f8ac7d73ce6
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-13 12:00:00 +0530

    commit

---
+++ SortList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        ListNode temp = head;
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(temp!=null){
+            arr.add(temp.val);
+            temp = temp.next;
+        }
+        Collections.sort(arr);
+        ListNode t = head;
+        int i = 0;
+        while(i<arr.size()&&t!=null){
+            t.val = arr.get(i);
+            i++;
+            t = t.next;
+        }
+        return head;
+    }
+}
