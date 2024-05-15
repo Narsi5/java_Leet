e873fc77b5b02879fb40d21e9442661b4ea52592
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-15 21:49:00 +0530

    problem no 822

---
+++ CardFlipGame.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CardFlipGame {
+    public int flipgame(int[] fronts, int[] backs) {
+        int n = fronts.length;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (fronts[i] == backs[i]) {
+                count.put(fronts[i], count.getOrDefault(fronts[i], 0) + 1);
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (!count.containsKey(fronts[i])) {
+                res = Math.min(res, fronts[i]);
+            }
+            if (!count.containsKey(backs[i])) {
+                res = Math.min(res, backs[i]);
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
