3110e051051a2f9880636a0b4f1ab6c5f1a55c64
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-05-18 12:24:24 +0530

    problem no 148

--- SortList.java
+++ SortList.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 public class SortList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode sortList(ListNode head) {
 
         if(head == null || head.next == null){
