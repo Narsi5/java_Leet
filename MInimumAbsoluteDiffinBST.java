7d6c9005ce23af212bdc7ae504416cd4df3469b6
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2024-06-28 20:05:54 +0530

    problem no 530

---
+++ MInimumAbsoluteDiffinBST.java
@@ -0,0 +1,18 @@
+public class MInimumAbsoluteDiffinBST {
+    int diff = Integer.MAX_VALUE;
+    int prev = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+
+        helper(root);
+        return diff;
+    }
+    public void helper(TreeNode root){
+        if(root == null){
+            return;
+        }
+        helper(root.left);
+        diff = Math.min(diff,Math.abs(root.val -prev));
+        prev = root.val;
+        helper(root.right);
+    }
+}
