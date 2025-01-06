b39b159d7c55adfe4e4cb43ffd4e2efdb8b13ea3
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2025-01-06 17:52:43 +0530

    Problem no 1769

---
+++ MinimumNumberofOperationstoMoveAllBallstoEachBox.java
@@ -0,0 +1,17 @@
+public class MinimumNumberofOperationstoMoveAllBallstoEachBox {
+    public int[] minOperations(String boxes) {
+        int[] ans = new int[boxes.length()];
+        for(int i = 0;i<boxes.length();i++){
+
+            for(int j =0;j<boxes.length();j++){
+                if(boxes.charAt(j) == '1'){
+                    ans[i]+= Math.abs(j-i);
+                }
+            }
+
+
+        }
+        return ans;
+    }
+
+}
