5e83da608bcb44aee4903436f8f0d860640d5fe0
Narsi5 <narasimhareddyporeddy1893@gmail.com>
2025-01-15 08:24:23 +0530

    Resolved the naming

--- rS.java
+++ rS.java
@@ -1,10 +1,13 @@
-public class reverseString {
+public class rS {
     public static void printreverse(char[] s){
         int n = s.length;
         for(int i = 0;i<n/2;i++){
             char temp = s[i];
             s[i]=s[n-1-i];
             s[n-1-i] = temp;
+        }
+        for(int i = 0;i<n;i++){
+            System.out.print(s[i]+" ");
         }
     }
     public static void main(String[] args) {
